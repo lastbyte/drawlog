88c9f3809e8f18ee13d3ab7a184d16d4263d5027
Nishant Kumar <nishant.9c9@gmail.com>
2025-10-04 05:24:11 +0530

    feat: replace BrowserRouter with HashRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import App from "./App.tsx";
 import { store } from "./store/index.ts";
@@ -9,9 +9,9 @@ import { store } from "./store/index.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
